c709ea7b50a4acb67198df56a059fc171e486447
anarkia1976 <stefano.villa1976@gmail.com>
2015-11-29 08:45:59 +0100

    ukm: added all necessary files

---
+++ data/UKM/device/.device.sh
@@ -0,0 +1,20 @@
+#!/system/bin/sh
+
+UKM=/data/UKM;
+BB=$UKM/busybox;
+DEVPROP="ro.product.device";
+DEVICE=`getprop "$DEVPROP" 2> /dev/null`;
+
+if [ -z "$DEVICE" ]; then DEVICE=`$BB grep "$DEVPROP=" /system/build.prop | $BB cut -d= -f2`; fi;
+
+#Official
+case $DEVICE in
+	OnePlus2|A2001|A2003|A2005)
+		CONFIG="oneplus2";; #OnePlus Two
+	*)
+		CONFIG="";; #Not defined
+esac;
+
+if [ -n "$CONFIG" ]; then PATH="$UKM/device/$CONFIG.sh"; else PATH=""; fi;
+
+$BB echo "$PATH";
