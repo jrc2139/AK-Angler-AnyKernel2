d88d4c0a32b6c3a158f2fea157152f8ce800ef9d
anarkia1976 <stefano.villa1976@gmail.com>
2016-01-02 17:48:03 +0100

    anyKernel2: initialize frequency limits

--- anykernel.sh
+++ anykernel.sh
@@ -198,6 +198,23 @@ dump_boot;
 
 # begin ramdisk changes
 
+# initialize frequency limits
+backup_file init.rc;
+insert_line init.rc "# initialize frequency limits" after "chmod 0660 /sys/power/wake_unlock" "    write /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq_hardlimit 1958400";
+insert_line init.rc "# initialize frequency limits" after "chmod 0660 /sys/power/wake_unlock" "    chmod 0664 /sys/devices/system/cpu/cpu4/cpufreq/scaling_max_freq_hardlimit";
+insert_line init.rc "# initialize frequency limits" after "chmod 0660 /sys/power/wake_unlock" "    chown system system /sys/devices/system/cpu/cpu4/cpufreq/scaling_max_freq_hardlimit";
+insert_line init.rc "# initialize frequency limits" after "chmod 0660 /sys/power/wake_unlock" "    write /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq_hardlimit 384000";
+insert_line init.rc "# initialize frequency limits" after "chmod 0660 /sys/power/wake_unlock" "    chmod 0664 /sys/devices/system/cpu/cpu4/cpufreq/scaling_min_freq_hardlimit";
+insert_line init.rc "# initialize frequency limits" after "chmod 0660 /sys/power/wake_unlock" "    chown system system /sys/devices/system/cpu/cpu4/cpufreq/scaling_min_freq_hardlimit";
+insert_line init.rc "# initialize frequency limits" after "chmod 0660 /sys/power/wake_unlock" "    write /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq_hardlimit 1555200";
+insert_line init.rc "# initialize frequency limits" after "chmod 0660 /sys/power/wake_unlock" "    chmod 0664 /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq_hardlimit";
+insert_line init.rc "# initialize frequency limits" after "chmod 0660 /sys/power/wake_unlock" "    chown system system /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq_hardlimit";
+insert_line init.rc "# initialize frequency limits" after "chmod 0660 /sys/power/wake_unlock" "    write /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq_hardlimit 384000";
+insert_line init.rc "# initialize frequency limits" after "chmod 0660 /sys/power/wake_unlock" "    chmod 0664 /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq_hardlimit";
+insert_line init.rc "# initialize frequency limits" after "chmod 0660 /sys/power/wake_unlock" "    chown system system /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq_hardlimit";
+insert_line init.rc "# initialize frequency limits" after "chmod 0660 /sys/power/wake_unlock" "    # initialize frequency limits";
+insert_line init.rc "# initialize frequency limits" after "chmod 0660 /sys/power/wake_unlock" "";
+
 # end ramdisk changes
 
 write_boot;
